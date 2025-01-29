6f8c033cbeacd7f345e0460a755b6463ebb03a54
Muhammad Uzair <uzair@192.168.1.2>
2025-01-29 16:36:20 +0500

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+import About from "./pages/about";
+import SignUp from "./pages/SignUp";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up " element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
